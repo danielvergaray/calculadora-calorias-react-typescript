16cd40456bad7f4d39818f04f5a2bca375096def
Daniel Vergaray <danielvergarayescobar@gmail.com>
2024-08-08 11:54:24 -0500

    culminado

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Form from "./components/Form"
 import { useReducer, useEffect, useMemo } from "react"
 import { activityReducer, initialState } from "./reducers/activivy-reducer"
 import ActivityList from "./components/ActivityList"
+import CalorieTracker from "./components/CalorieTracker"
 
 function App() {
 
@@ -16,7 +17,7 @@ function App() {
   return (
     <>
       <header className="bg-lime-600 py-3">
-          <div className="max-w-4xl mx-auto flex justify-between">
+          <div className="max-w-4xl mx-auto flex justify-between items-center">
               <h1 className="text-center text-lg font-bold text-white uppercase">Contador de Calorias</h1>
               <button className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10"
               disabled={!canRestartApp()} 
@@ -33,6 +34,14 @@ function App() {
             dispatch={dispatch}
             state={state}
             />
+        </div>
+      </section>
+
+      <section className="bg-gray-800 py-10">
+        <div className="max-w-4xl mx-auto">
+          <CalorieTracker
+            activities = { state.activities }
+          />
         </div>
       </section>
 
